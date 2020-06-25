dee0bfa62db38c17e70e4bacb8a744b1a37cfbc8
Danish Tamboli <danishtamboli123@gmail.com>
2020-06-25 20:45:20 +0530

    Updated Website to match with 2020 Season.
    Signed-off-by: Danish Tamboli <danishtamboli123@gmail.com>

---
+++ payment.php
@@ -0,0 +1,193 @@
+<?php
+	header("Pragma: no-cache");
+	header("Cache-Control: no-cache");
+	header("Expires: 0");
+
+?>
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="STES Racing" content="The offical website of STES Racing">
+    <meta name="STES" content="Sinhgad Technical Education Society">
+    <meta name="Racing" content="Fsae Racing Team of STES">
+    <meta name="Sinhgad" content="College Location">
+    <meta name="Sinhgad College" content="FSAE Team">
+    <meta name="Sinhgad College of Engineering" content="FSAE Team">
+    <meta name="Sinhgad Technical Education Society" content="FSAE Team">
+    <meta name="FSAE" content="Formula Society of Automotive Engineers">
+    <meta name="Formula" content="FSAE">
+    <meta name="Formula Society of Automotive Engineers" content="FSAE">
+    <meta name="Formula Bharat" content="FSAE">
+    <meta name="Supra" content="FSAE">
+     <meta name="keywords" content="Singhad,Sinhgad College,FSAE,Formula SAE,Formula Bharat,Supra,Baja,Formula Society of Automotive Engineers,Student,Student Team,STES,Sinhgad Technical Education Society,">
+    <meta name="description" content="STES Racing is Sinhgad Technical Education Society’s Formula SAE Team, consisting of undergraduate students with the goal to build world class combustion vechicles.">
+    <meta name="GENERATOR" content="Evrsoft First Page">
+    <title>Payment — STES Racing</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bitter:400,700">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Didact+Gothic">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Voltaire">
+    <link rel="stylesheet" href="assets/css/styles.min.css">
+    <link rel="apple-touch-icon" sizes="57x57" href="assets/img/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="assets/img/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="assets/img/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="assets/img/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="assets/img/apple-icon-120x120.png">	
+    <link rel="apple-touch-icon" sizes="144x144" href="assets/img/apple-icon-144x144.png">	
+    <link rel="apple-touch-icon" sizes="152x152" href="assets/img/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/img/apple-icon-180x180.png">	
+    <link rel="icon" type="image/png" sizes="192x192"  href="assets/img/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="assets/img/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicon-16x16.png">
+    <link rel="manifest" href="/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+    <style>
+    .no-js #loader { display: none;  }
+.js #loader { display: block; position: absolute; left: 100px; top: 0; }
+.se-pre-con {
+	position: fixed;
+	left: 0px;
+	top: 0px;
+	width: 100%;
+	height: 100%;
+	z-index: 9999;
+	background: url(assets/img/display_loading.gif) center no-repeat #fff;
+	background-color: #919191;
+}
+</style>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.js"></script>
+<script>
+$(window).load(function() {
+		// Animate loader off screen
+		setTimeout(function() {
+  $(".se-pre-con").fadeOut("slow");;
+}, 1000);
+	});
+</script>
+</head>
+<div class="se-pre-con"></div>
+<body style="background-color:rgb(0,0,0);color:rgb(255,255,255);overflow-wrap: break-word;">
+    <div>
+        <div class="header-dark" style="background-image:url(&quot;assets/img/IMG_20180714_1508361.jpg&quot;);height:100vh;color:rgb(255,255,255);background-position:center;">
+            <div style="height:100vh;background-color:rgba(0,0,0,0.5);">
+                <nav class="navbar navbar-dark navbar-expand-md navigation-clean-search">
+                    <div class="container-fluid"><a class="navbar-brand" href="/"><img src="assets/img/Logo White.png" alt="STES Racing Logo" width="100%" style="height:32px;width:auto;"></a><button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-1" style="border:none;"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+                        <div
+                            class="collapse navbar-collapse" id="navcol-1">
+                            <ul class="nav navbar-nav"></ul>
+                            <form class="form-inline mr-auto" target="_self">
+                                <div class="form-group"><label for="search-field"></label></div>
+                            </form><span class="navbar-text"></span>
+                            <ul class="nav navbar-nav">
+                                <li class="nav-item" role="presentation"><a class="nav-link" href="About-us" style="color:rgb(255,255,255);">ABOUT US</a></li>
+                                <li class="nav-item" role="presentation"><a class="nav-link" href="Team">TEAM</a></li>
+                                <li class="nav-item" role="presentation"><a class="nav-link" href="Cars">CARS</a></li>
+                                <li class="nav-item" role="presentation"><a class="nav-link" href="Sponsors">SPONSORS</a></li>
+                                <li class="nav-item" role="presentation"><a class="nav-link" href="FSAE">FSAE</a></li>
+                                <li class="nav-item" role="presentation"><a class="nav-link" href="Contact">CONTACT US</a></li>
+                            </ul>
+                    </div>
+            </div>
+            </nav>
+            <h1 class="text-center" style="font-size:calc(1em + 5vw);padding-top:5vh;padding-bottom:5vh;">SPONSOR US</h1>
+            <div>
+ <div>
+        <div class="container">
+            <div class="row justify-content-center" style="padding-bottom:2vh;">
+                <div class="col-6 col-sm-4 col-md-3 col-lg-2 col-xl-2 offset-0 offset-sm-0 offset-md-0 offset-lg-0" style="padding:0px;"><img src="assets/img/Paytm.png" width="100%"></div>
+            </div>
+        </div>
+    </div>
+                <div class="container">
+                    <div class="row justify-content-center align-items-center">
+                        <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-9">
+                            <div class="table-responsive">
+                                <table class="table table-dark" style="border-radius:1em;opacity:0.9;" >
+                                    <thead>
+                                    </thead>
+			
+	<form method="post" action="pgRedirect">
+                                    <tbody>
+                                        <tr>
+                                            <td style="text-align:center;font-size:calc(1em + 0.2vw);padding-top:25px;">TRANSACTION NUMBER</td>
+                                            <td style="text-align:center;padding-top:25px;"><input id="ORDER_ID" readonly tabindex="1" maxlength="12" size="12" name="ORDER_ID" autocomplete="off" value="<?php echo  "SR" . date('ymd') . rand(1,9999) ?>"readonly="" style="text-align:center;font-size:calc(1em + 0.2vw);border-radius:1em;border:none;"></td>
+                                        </tr>
+                                        <tr>
+                                            <td style="text-align:center;font-size:calc(1em + 0.2vw);">CUSTOMER ID</td>
+                                            <td style="text-align:center;"><input id="CUST_ID" readonly tabindex="2" maxlength="12" size="12" name="CUST_ID" autocomplete="off" value="<?php echo  "STES" . rand(1,99999999)?>" readonly="" style="text-align:center;font-size:calc(1em + 0.2vw);border-radius:1em;border:none;"></td>
+                                        </tr>
+                                        <tr>
+                                            <td style="text-align:center;font-size:calc(1em + 0.2vw);">PAYMENT TYPE</td>
+                                            <td style="text-align:center;"><input id="INDUSTRY_TYPE_ID" readonly tabindex="4" maxlength="12" size="12" name="INDUSTRY_TYPE_ID" autocomplete="off" value="Retail" readonly="" style="text-align:center;font-size:calc(1em + 0.2vw);border-radius:1em;border:none;"></td>
+                                        </tr>
+                                        <tr>
+                                            <td style="text-align:center;font-size:calc(1em + 0.2vw);">CHANNEL</td>
+                                            <td style="text-align:center;"><input id="CHANNEL_ID" readonly tabindex="4" maxlength="12"
+						size="12" name="CHANNEL_ID" autocomplete="off" value="WEB" style="text-align:center;font-size:calc(1em + 0.2vw);border-radius:1em;border:none;"></td>
+                                        </tr>
+                                        <tr>
+                                            <td style="text-align:center;font-size:calc(1em + 0.2vw);padding-bottom:25px;">DONATION AMOUNT</td>
+                                            <td style="text-align:center;padding-bottom:25px;"><input title="TXN_AMOUNT" onkeyup="this.value=this.value.replace(/[^\d]+/,'')" tabindex="10"
+						type="tel" minlength="1" maxlength="7" size="12" name="TXN_AMOUNT"
+						value="" required placeholder="INR (₹)" style="text-align:center;font-size:calc(1em + 0.2vw);border-radius:1em;border:none;" oninvalid="this.setCustomValidity('Please Enter The Amount you wish to Donate.')" oninput="setCustomValidity('')"></td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                                 <div class="highlight-blue" style="background-color:rgba(0,0,0,0);padding-top:0px;padding-bottom:0px;">
+        <div class="container">
+            <div class="intro"></div>
+            <div class="buttons"><button class="btn btn-primary btn-lg" type="submit" onclick="">CONTRIBUTE</button></div>
+        </div>
+    </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+	</form>
+                        <div class="footer-dark" style="padding-top:10vh;padding-bottom:0;background-color:rgb(0,0,0);">
+        <footer>
+            <div class="container" style="padding-top:25px;padding-bottom:25px;">
+                <div class="row">
+                    <div class="col-sm-6 col-md-3 offset-sm-3 offset-xl-3 item">
+                        <h3>ABOUT</h3>
+                        <ul>
+                            <li><a href="Team">Team</a></li>
+                            <li><a href="Cars">Cars</a></li>
+                            <li><a href="Sponsors">Sponsors</a></li>
+                            <li><a href="FSAE">What is FSAE?</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-6 item text">
+                        <h3><a href="Contact" style="color:rgb(255,255,255);">CONTACT US</a></h3>
+                        <p>STES Racing</p>
+                        <p>Singhad College of Engineering,</p>
+                        <p>Ambegaon BK,Pune</p>
+                        <p>Maharashtra 411041,India</p>
+                        <p>Email ID: stesracing@gmail.com</p>
+                    </div>
+                </div>
+                <div class="col item social" style="margin-top:12px;"><a href="https://www.facebook.com/stesracingteam/"><i class="icon ion-social-facebook" style="color:#00ff00;"></i></a><a href="https://www.instagram.com/stesracing"><i class="icon ion-social-instagram" style="color:#00ff00;"></i></a>
+                    <a
+                        href="https://twitter.com/stesracing"><i class="icon ion-social-twitter" style="color:#00ff00;"></i></a><a href="https://www.linkedin.com/company/stes-racing"><i class="icon ion-social-linkedin" style="color:#00ff00;"></i></a></div>
+                <p class="copyright">STES Racing © <?php echo date('yy') ?></p>
+            </div>
+        </footer>
+    </div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/script.min.js"></script>
+</body>
+</html>
---
+++ pgResponse.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="STES Racing" content="The offical website of STES Racing">
+    <meta name="STES" content="Sinhgad Technical Education Society">
+    <meta name="Racing" content="Fsae Racing Team of STES">
+    <meta name="Sinhgad" content="College Location">
+    <meta name="Sinhgad College" content="FSAE Team">
+    <meta name="Sinhgad College of Engineering" content="FSAE Team">
+    <meta name="Sinhgad Technical Education Society" content="FSAE Team">
+    <meta name="FSAE" content="Formula Society of Automotive Engineers">
+    <meta name="Formula" content="FSAE">
+    <meta name="Formula Society of Automotive Engineers" content="FSAE">
+    <meta name="Formula Bharat" content="FSAE">
+    <meta name="Supra" content="FSAE">
+    <meta name="GENERATOR" content="Evrsoft First Page">
+    <meta name="keywords" content="Singhad,Sinhgad College,FSAE,Formula SAE,Formula Bharat,Supra,Baja,Formula Society of Automotive Engineers,Student,Student Team,STES,Sinhgad Technical Education Society,">
+    <meta name="description" content="STES Racing is Sinhgad Technical Education Society’s Formula SAE Team, consisting of undergraduate students with the goal to build world class combustion vechicles.">
+    <title>Payment — STES Racing</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bitter:400,700">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Didact+Gothic">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Voltaire">
+    <link rel="stylesheet" href="assets/css/styles.min.css">
+    <link rel="apple-touch-icon" sizes="57x57" href="assets/img/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="assets/img/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="assets/img/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="assets/img/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="assets/img/apple-icon-120x120.png">	
+    <link rel="apple-touch-icon" sizes="144x144" href="assets/img/apple-icon-144x144.png">	
+    <link rel="apple-touch-icon" sizes="152x152" href="assets/img/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/img/apple-icon-180x180.png">	
+    <link rel="icon" type="image/png" sizes="192x192"  href="assets/img/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="assets/img/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicon-16x16.png">
+    <link rel="manifest" href="/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+    <style>
+    .no-js #loader { display: none;  }
+.js #loader { display: block; position: absolute; left: 100px; top: 0; }
+.se-pre-con {
+	position: fixed;
+	left: 0px;
+	top: 0px;
+	width: 100%;
+	height: 100%;
+	z-index: 9999;
+	background: url(assets/img/display_loading.gif) center no-repeat #fff;
+	background-color: #919191;
+}
+</style>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.js"></script>
+<script>
+$(window).load(function() {
+		// Animate loader off screen
+		setTimeout(function() {
+  $(".se-pre-con").fadeOut("slow");;
+}, 1000);
+	});
+</script>
+</head>
+<div class="se-pre-con"></div>
+<body style="background-color:rgb(0,0,0);color:rgb(255,255,255);overflow-wrap: break-word;">
+    <div>
+        <div class="header-dark" style="background-image:url(&quot;assets/img/IMG_20180714_1508361.jpg&quot;);height:100vh;color:rgb(255,255,255);background-position:center;">
+            <div style="height:100vh;background-color:rgba(0,0,0,0.5);">
+                <nav class="navbar navbar-dark navbar-expand-md navigation-clean-search">
+                    <div class="container-fluid"><a class="navbar-brand" href="/"><img src="assets/img/Logo White.png" alt="STES Racing Logo" width="100%" style="height:32px;width:auto;"></a><button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-1" style="border:none;"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+                        <div
+                            class="collapse navbar-collapse" id="navcol-1">
+                            <ul class="nav navbar-nav"></ul>
+                            <form class="form-inline mr-auto" target="_self">
+                                <div class="form-group"><label for="search-field"></label></div>
+                            </form><span class="navbar-text"></span>
+                            <ul class="nav navbar-nav">
+                                <li class="nav-item" role="presentation"><a class="nav-link" href="about-us" style="color:rgb(255,255,255);">ABOUT US</a></li>
+                                <li class="nav-item" role="presentation"><a class="nav-link" href="Team">TEAM</a></li>
+                                <li class="nav-item" role="presentation"><a class="nav-link" href="Cars">CARS</a></li>
+                                <li class="nav-item" role="presentation"><a class="nav-link" href="Sponsors">SPONSORS</a></li>
+                                <li class="nav-item" role="presentation"><a class="nav-link" href="FSAE">FSAE</a></li>
+                                <li class="nav-item" role="presentation"><a class="nav-link" href="Contact">CONTACT US</a></li>
+                            </ul>
+                    </div>
+            </div>
+            </nav>
+<?php
+
+
+// following files need to be included
+require_once("./lib/config_paytm.php");
+require_once("./lib/encdec_paytm.php");
+$paytmChecksum = "";
+$paramList = array();
+$isValidChecksum = "FALSE";
+
+$paramList = $_POST;
+$paytmChecksum = isset($_POST["CHECKSUMHASH"]) ? $_POST["CHECKSUMHASH"] : ""; //Sent by Paytm pg
+
+//Verify all parameters received from Paytm pg to your application. Like MID received from paytm pg is same as your applications MID, TXN_AMOUNT and ORDER_ID are same as what was sent by you to Paytm PG for initiating transaction etc.
+$isValidChecksum = verifychecksum_e($paramList, PAYTM_MERCHANT_KEY, $paytmChecksum); //will return TRUE or FALSE string.
+
+
+if($isValidChecksum == "TRUE") {
+	if ($_POST["STATUS"] == "TXN_SUCCESS") {
+		echo "<div style='text-align:center;font-size:calc(1em + 5vw);padding-top:5vh;padding-bottom:5vh;'>" . "<b>Transaction is Successful</b>". "</div>"  . "<br/>";
+		//Process your transaction here as success transaction.
+		//Verify amount & order id received from Payment gateway with your application's order id and amount.
+	}
+	else {echo "<div style='text-align:center;font-size:calc(1em + 5vw);padding-top:5vh;padding-bottom:5vh;'>" . "<b>Transaction is Unsucessful</b>". "</div>" . "<br/>";
+		
+	}
+				echo  '<div class="container">' . '<div class="row justify-content-center align-items-center">' . '<div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-9" style="position:relative;top:-5vh">' ;
+	if (isset($_POST) && count($_POST)>0 )
+	{			echo '<div class="table-responsive">' . '<table class="table table-dark" style="border-radius:3vh">';
+				echo '<thead>';
+				echo '</thead>';
+				echo '<tbody>';
+		foreach($_POST as $paramName => $paramValue) {
+				if ($paramName == 'CHECKSUMHASH') continue;
+				if ($paramName == 'TXNID') continue;
+				if ($paramName == 'MID') continue;
+				if ($paramName == 'PAYMENTMODE') continue;
+				if ($paramName == 'RESPCODE') continue;
+				if ($paramName == 'RESPMSG') continue;
+				echo '<tr>' ;
+				echo '<td style="text-align:center;font-size:calc(1em + 0.2vw)">' . $paramName . '</td>' ;
+				echo '<td style="text-align:center;font-size:calc(1em + 0.2vw);">' . $paramValue . '</td>' ;
+				echo '</tr>' ;
+	
+		}
+				echo '</tbody>';
+				echo '</table>' . '</div>'. '</div>'. '</div>'. '</div>' . '</div>' .'</div>' ;
+	}
+	
+
+}
+else {
+	echo "<b>Checksum mismatched.</b>";
+	//Process transaction as suspicious.
+}
+?>
+ <div class="footer-dark" style="padding-top:0;padding-bottom:0;background-color:rgb(0,0,0);">
+        <footer>
+            <div class="container" style="padding-top:25px;padding-bottom:25px;">
+                <div class="row">
+                    <div class="col-sm-6 col-md-3 offset-sm-3 offset-xl-3 item">
+                        <h3>ABOUT</h3>
+                        <ul>
+                            <li><a href="Team">Team</a></li>
+                            <li><a href="Cars">Cars</a></li>
+                            <li><a href="Sponsors">Sponsors</a></li>
+                            <li><a href="FSAE">What is FSAE?</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-6 item text">
+                        <h3><a href="Contact" style="color:rgb(255,255,255);">CONTACT US</a></h3>
+                        <p>STES Racing</p>
+                        <p>Singhad College of Engineering,</p>
+                        <p>Ambegaon BK,Pune</p>
+                        <p>Maharashtra 411041,India</p>
+                        <p>Email ID: stesracing@gmail.com</p>
+                    </div>
+                </div>
+                <div class="col item social" style="margin-top:12px;"><a href="https://www.facebook.com/stesracingteam/"><i class="icon ion-social-facebook" style="color:#00ff00;"></i></a><a href="https://www.instagram.com/stesracing"><i class="icon ion-social-instagram" style="color:#00ff00;"></i></a>
+                    <a
+                        href="https://twitter.com/stesracing"><i class="icon ion-social-twitter" style="color:#00ff00;"></i></a><a href="https://www.linkedin.com/company/stes-racing"><i class="icon ion-social-linkedin" style="color:#00ff00;"></i></a></div>
+                <p class="copyright">STES Racing © <?php echo date('yy') ?></p>
+            </div>
+        </footer>
+    </div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/script.min.js"></script>
+</body>
+</html>
